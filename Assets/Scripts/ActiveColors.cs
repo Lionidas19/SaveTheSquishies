4652a2b47ec92e0f2a435ac8240f4407b4f88694
Lionidas19 <leon.katsaros@gmail.com>
2021-03-11 15:37:31 +0200

    First commit - Code

---
+++ Assets/Scripts/ActiveColors.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ActiveColors
+{
+    public static bool yellow = false;
+    public static bool blue = false;
+    public static bool red = false;
+
+    public static bool goButton = false;
+}
---
+++ Assets/Scripts/AvatarMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ActiveColors.goButton == true)
+        transform.Translate(Vector2.right * Time.deltaTime * movementSpeed);
+    }
+}
