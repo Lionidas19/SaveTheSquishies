016787102f34dd0c1e486c95073f26a549110a31
Lionidas19 <leon.katsaros@gmail.com>
2021-05-13 20:39:23 +0300

    Fixed level reset

---
+++ Assets/Scripts/TransitionDoorAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionDoorAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ActiveButtons.resetButton == true)
+        {
+            ActiveButtons.resetButton = false;
+            animator.Play("LevelClose");
+            Invoke("ResetScene", animator.GetCurrentAnimatorStateInfo(0).length);
+        }
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
