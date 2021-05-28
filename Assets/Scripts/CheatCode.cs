dfe164710e9635664d25207b767717641e8a8ccd
microdiffuse <christian.paller@mac.com>
2021-05-29 00:01:53 +0200

    added cheatcode

---
+++ Assets/Scripts/CheatCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheatCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  if (Input.GetKey("b") && Input.GetKey("u") && Input.GetKey("r") && Input.GetKey("n"))
+        if (Input.GetKey("space") && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))
+        {
+            SceneManager.LoadScene("TitleScreen End");
+        }
+    }
+}
